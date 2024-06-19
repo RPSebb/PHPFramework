76a97ea859e3cd637562cbb374e85503f0cf4217
RPSebb <sebastien.ranganayaguy@live.fr>
2024-06-19 16:04:45 +0400

    Delete useless lines in ArrayVisualizer

--- framework/ArrayVisualizer.php
+++ framework/ArrayVisualizer.php
@@ -1,9 +1,6 @@
 <?php
 class ArrayVisualizer {
 
-    protected $space = '&emsp;';
-    protected $jl = '<br>';
-
     function print($arr, $depth = 0) {
         $filled = !empty($arr);
         $keys = array_keys($arr);
@@ -28,33 +25,6 @@ class ArrayVisualizer {
         if($filled) { str_repeat('&emsp;', $depth); }
         echo(str_repeat('&emsp;', $depth).'}<br>');
     }
-
-    // function convert(array $arr, int $depth = 0) : string {
-    //     $text = '{'.$this->jl;
-    //     $lastKey = array_key_last($arr);
-
-    //     foreach($arr as $key => $value) {
-    //         $text .= str_repeat($this->space, $depth + 1) . "$key" . ' : ';
-    //         if(is_array($value)) {
-    //             $text .= $this->convert($value, $depth + 1);
-    //             $jump = false;
-    //         } else {
-    //             $jump = true;
-    //             try {
-    //                 $text .= $value;
-    //             } catch (\Throwable $th) {
-    //                 $text .= 'unknown';
-    //             }
-    //         }
-
-    //         if($key !== $lastKey) { $text .= ','; }
-    //         if($jump) { $text .= $this->jl; }
-
-    //     }
-
-    //     $text .= str_repeat($this->space, $depth). '}' . $this->jl;
-    //     return $text;
-    // }
 
     function json(array $arr) : string {
         $text = '{';
